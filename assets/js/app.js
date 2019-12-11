723ffa5412bc592a9839a14416e8be85fd64cb73
super1002 <flexiway.studio@gmail.com>
2019-12-11 19:21:35 +0100

    some more setup

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,8 +7,8 @@
 
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
+import 'bootstrap';
+import '../scss/main.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
-
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
